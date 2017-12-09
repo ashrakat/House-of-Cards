fbce79db01d919a1c067a5d47fa20ded9c37dfa7
NooorMohamed <noormohamed.28.96@gmail.com>
2017-12-10 00:53:58 +0200

    Add files via upload

--- src/Models/House.java
+++ src/Models/House.java
@@ -12,6 +12,7 @@ public class House {
 	
 	/***forgetten Location **/
 	
+	int id ; 
 	int size;
 	double rate;
 	int pics[][];
@@ -103,117 +104,17 @@ public class House {
 		this.comments = comments;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	
 
-<<<<<<< HEAD
-	
-=======
->>>>>>> 6a80f21ab2143ee79dfbbfe4e8cbc51334d90cda
-	
-
-}package Models;
-
-import java.util.ArrayList;
-
-public class House {
-	
-	User owner;
-	String type; //Villa, Studi
-	String status; //finished or half finished
-	String forWhat;
-	String description;
-	
-	/***forgetten Location **/
-	
-	int size;
-	double rate;
-	int pics[][];
-	int numOfFloors;
-	ArrayList<Comment> comments = new ArrayList<Comment>();
 	
 	
-	public House() {
-		
-	}
-
-	public User getOwner() {
-		return owner;
-	}
-
-	public void setOwner(User owner) {
-		this.owner = owner;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getForWhat() {
-		return forWhat;
-	}
-
-	public void setForWhat(String forWhat) {
-		this.forWhat = forWhat;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public int getSize() {
-		return size;
-	}
-
-	public void setSize(int size) {
-		this.size = size;
-	}
-
-	public double getRate() {
-		return rate;
-	}
-
-	public void setRate(double rate) {
-		this.rate = rate;
-	}
-
-	public int[][] getPics() {
-		return pics;
-	}
-
-	public void setPics(int[][] pics) {
-		this.pics = pics;
-	}
-
-	public int getNumOfFloors() {
-		return numOfFloors;
-	}
-
-	public void setNumOfFloors(int numOfFloors) {
-		this.numOfFloors = numOfFloors;
-	}
-
-	public ArrayList<Comment> getComments() {
-		return comments;
-	}
-
-	public void setComments(ArrayList<Comment> comments) {
-		this.comments = comments;
-	}
 
 }
