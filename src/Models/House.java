45049dfff3edb583805a358ceb891ab9ec487af4
NooorMohamed <noormohamed.28.96@gmail.com>
2017-12-09 16:25:24 +0200

    Add files via upload

--- src/Models/House.java
+++ src/Models/House.java
@@ -10,6 +10,8 @@ public class House {
 	String forWhat;
 	String description;
 	
+	/***forgetten Location **/
+	
 	int size;
 	double rate;
 	int pics[][];
@@ -21,107 +23,88 @@ public class House {
 		
 	}
 
-
 	public User getOwner() {
 		return owner;
 	}
-
 
 	public void setOwner(User owner) {
 		this.owner = owner;
 	}
 
-
 	public String getType() {
 		return type;
 	}
-
 
 	public void setType(String type) {
 		this.type = type;
 	}
 
-
 	public String getStatus() {
 		return status;
 	}
-
 
 	public void setStatus(String status) {
 		this.status = status;
 	}
 
-
 	public String getForWhat() {
 		return forWhat;
 	}
-
 
 	public void setForWhat(String forWhat) {
 		this.forWhat = forWhat;
 	}
 
-
 	public String getDescription() {
 		return description;
 	}
-
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
 	public int getSize() {
 		return size;
 	}
-
 
 	public void setSize(int size) {
 		this.size = size;
 	}
 
-
 	public double getRate() {
 		return rate;
 	}
-
 
 	public void setRate(double rate) {
 		this.rate = rate;
 	}
 
-
 	public int[][] getPics() {
 		return pics;
 	}
-
 
 	public void setPics(int[][] pics) {
 		this.pics = pics;
 	}
 
-
 	public int getNumOfFloors() {
 		return numOfFloors;
 	}
-
 
 	public void setNumOfFloors(int numOfFloors) {
 		this.numOfFloors = numOfFloors;
 	}
 
-
 	public ArrayList<Comment> getComments() {
 		return comments;
 	}
 
-
 	public void setComments(ArrayList<Comment> comments) {
 		this.comments = comments;
 	}
+
 	
-	
+
 	
 	
 
--- src/Models/Normal.java
+++ src/Models/Normal.java
@@ -5,5 +5,43 @@ import java.util.ArrayList;
 public class Normal extends User{
 	
 	ArrayList<House> listOfHouses = new ArrayList<House>();
+	ArrayList<Notifications> listOfNotifications= new ArrayList<Notifications>();
+	ArrayList <String> Preferences = new ArrayList <String>() ; 
+
+	public ArrayList<House> getListOfHouses() {
+		return listOfHouses;
+	}
+
+	public void setListOfHouses(ArrayList<House> listOfHouses) {
+		this.listOfHouses = listOfHouses;
+	}
 	
+	public ArrayList<Notifications> getListOfNotifications() {
+		return listOfNotifications;
+	}
+
+	public void setListOfNotifications(ArrayList<Notifications> listOfNotifications) {
+		this.listOfNotifications = listOfNotifications;
+	}
+	
+	public ArrayList<String> getPreferences() {
+		return Preferences;
+	}
+
+	public void setPreferences(ArrayList<String> preferences) {
+		Preferences = preferences;
+	}
+
+	public void addHouse(House house){
+		this.listOfHouses.add(house);
+	}
+	
+	public void addNotification(Notifications Notify){
+		this.listOfNotifications.add(Notify);
+	}
+	
+	public void addPreferences(String Preference){
+		this.Preferences.add(Preference);
+	}
+
 }
