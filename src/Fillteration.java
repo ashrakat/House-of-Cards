6a80f21ab2143ee79dfbbfe4e8cbc51334d90cda
NooorMohamed <noormohamed.28.96@gmail.com>
2017-12-09 20:55:42 +0200

    Add files via upload

---
+++ src/Fillteration.java
@@ -0,0 +1,51 @@
+
+import Models.House ; 
+import java.util.*;
+
+public class Fillteration {
+	
+	public ArrayList<House> fillterByRentSale(double price , ArrayList<House>  listOfHouse) {
+		ArrayList<House> newList = new ArrayList<House>();
+		for(int i=0 ; i <listOfHouse.size() ; i++)
+			if(listOfHouse.get(i).getRate() == price)
+				newList.add(listOfHouse.get(i)); 
+		
+		return newList ; 
+	}
+	
+	/**when we put Location ask your team **/
+	public ArrayList<House> fillterByArea(double Area ) {	
+	
+	}
+
+	public ArrayList<House> fillterBySize(int size , ArrayList<House>  listOfHouse ) {
+		ArrayList<House> newList = new ArrayList<House>();
+		for(int i=0 ; i <listOfHouse.size() ; i++)
+			if(listOfHouse.get(i).getSize() == size)
+				newList.add(listOfHouse.get(i)); 
+		
+		return newList ; 
+	}
+	
+	public ArrayList<House> fillterByFloor(int numOfFloors , ArrayList<House>  listOfHouse ) {
+		ArrayList<House> newList = new ArrayList<House>();
+		for(int i=0 ; i <listOfHouse.size() ; i++)
+			if(listOfHouse.get(i).getNumOfFloors()== numOfFloors)
+				newList.add(listOfHouse.get(i)); 
+		
+		return newList ; 
+	}
+	
+	public ArrayList<House> fillterByType(String type , ArrayList<House>  listOfHouse ) {
+		ArrayList<House> newList = new ArrayList<House>();
+		for(int i=0 ; i <listOfHouse.size() ; i++)
+			if(listOfHouse.get(i).getType().equals(type))
+				newList.add(listOfHouse.get(i)); 
+		
+		return newList ; 
+	}
+	
+	
+
+	
+}
