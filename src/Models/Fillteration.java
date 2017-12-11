726a42cc7ec62cb584ed833c8a47ff93d800a288
NooorMohamed <noormohamed.28.96@gmail.com>
2017-12-11 16:01:53 +0200

    Add files via upload

--- src/Models/Fillteration.java
+++ src/Models/Fillteration.java
@@ -1,53 +1,62 @@
-package Models;
 
+import Models.Advertise;  
 import java.util.*;
 
 public class Fillteration {
 	
-	public ArrayList<Advertise> fillterByRentSale(double price , ArrayList<Advertise>  listOfHouse) {
+	public ArrayList<Advertise> fillterByRentSale(double rate , ArrayList<Advertise>  listOfAdvertise) {
 		ArrayList<Advertise> newList = new ArrayList<Advertise>();
-		for(int i=0 ; i <listOfHouse.size() ; i++)
-			if(listOfHouse.get(i).getRate() == price)
-				newList.add(listOfHouse.get(i)); 
+		for(int i=0 ; i <listOfAdvertise.size() ; i++)
+			if(listOfAdvertise.get(i).getRate() == rate)
+				newList.add(listOfAdvertise.get(i)); 
 		
 		return newList ; 
 	}
 	
-	/**when we put Location ask your team **/
-	public ArrayList<Advertise> fillterByArea(double Area, ArrayList<Advertise>  listOfHouse ) {	
+	public ArrayList<Advertise> fillterByArea(String Area, ArrayList<Advertise>  listOfAdvertise ) {	
 		ArrayList<Advertise> newList = new ArrayList<Advertise>();
+		for(int i=0 ; i <listOfAdvertise.size() ; i++)
+			if(listOfAdvertise.get(i).getAddress().equals(Area) )
+				newList.add(listOfAdvertise.get(i)); 
+		
+		return newList ; 
+	}
 
-		return newList ; 
-	}
-
-	public ArrayList<Advertise> fillterBySize(int size , ArrayList<Advertise>  listOfHouse ) {
+	public ArrayList<Advertise> fillterBySize(int size , ArrayList<Advertise>  listOfAdvertise ) {
 		ArrayList<Advertise> newList = new ArrayList<Advertise>();
-		for(int i=0 ; i <listOfHouse.size() ; i++)
-			if(listOfHouse.get(i).getSize() == size)
-				newList.add(listOfHouse.get(i)); 
+		for(int i=0 ; i <listOfAdvertise.size() ; i++)
+			if(listOfAdvertise.get(i).getSize() == size)
+				newList.add(listOfAdvertise.get(i)); 
 		
 		return newList ; 
 	}
 	
-	public ArrayList<Advertise> fillterByFloor(int numOfFloors , ArrayList<Advertise>  listOfHouse ) {
+	public ArrayList<Advertise> fillterByFloor(int numOfFloors , ArrayList<Advertise>  listOfAdvertise ) {
 		ArrayList<Advertise> newList = new ArrayList<Advertise>();
-		for(int i=0 ; i <listOfHouse.size() ; i++)
-			if(listOfHouse.get(i).getNumOfFloors()== numOfFloors)
-				newList.add(listOfHouse.get(i)); 
+		for(int i=0 ; i <listOfAdvertise.size() ; i++)
+			if(listOfAdvertise.get(i).getNumOfFloors()== numOfFloors)
+				newList.add(listOfAdvertise.get(i)); 
 		
 		return newList ; 
 	}
 	
-	public ArrayList<Advertise> fillterByType(String type , ArrayList<Advertise>  listOfHouse ) {
+	public ArrayList<Advertise> fillterByType(String type , ArrayList<Advertise>  listOfAdvertise ) {
 		ArrayList<Advertise> newList = new ArrayList<Advertise>();
-		for(int i=0 ; i <listOfHouse.size() ; i++)
-			if(listOfHouse.get(i).getType().equals(type))
-				newList.add(listOfHouse.get(i)); 
+		for(int i=0 ; i <listOfAdvertise.size() ; i++)
+			if(listOfAdvertise.get(i).getType().equals(type))
+				newList.add(listOfAdvertise.get(i)); 
 		
 		return newList ; 
 	}
 	
+	public ArrayList<Advertise> fillterByPrice(double price , ArrayList<Advertise>  listOfAdvertise ) {
+		ArrayList<Advertise> newList = new ArrayList<Advertise>();
+		for(int i=0 ; i <listOfAdvertise.size() ; i++)
+			if(listOfAdvertise.get(i).getPrice() == price)
+				newList.add(listOfAdvertise.get(i)); 
+		
+		return newList ; 
+	}
 	
-
 	
 }
